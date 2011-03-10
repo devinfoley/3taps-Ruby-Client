1aae1a6a646aafdb88611cb13e863ab6d9e855d3
moskva <moskva@machines.(none)>
2011-03-10 17:54:40 +0200

    search response

--- lib/dto/search/search_response.rb
+++ lib/dto/search/search_response.rb
@@ -2,6 +2,39 @@ class SearchResponse
   attr_accessor :success, :num_results, :exec_time_ms, :results
 
   def self.from_json(json)
-    
+    res = self.new
+    res.success = json["success"]
+    res.num_results = json["numResults"]
+    res.exec_time_ms = json["execTimeMs"]
+    res.results = []
+    json["results"].each do |posting|
+        res.results << Posting.new({
+        :postKey     => posting["postKey"],
+        :heading     => posting["heading"],
+        :body        => posting["body"], 
+        :category    => posting["category"], 
+        :source      => posting["source"], 
+        :location    => posting["location"],
+        :longitude   => posting["longitude"], 
+        :latitude    => posting["latitude"], 
+        :language    => posting["language"],
+        :price       => posting["price"],
+        :currency    => posting["currency"],
+        :externalURL => posting["externalURL"], 
+        :externalID  => posting["externalID"], 
+        :accountName => posting["accountName"], 
+        :accountID   => posting["accountID"],
+        :timestamp   => posting["timestamp"], 
+        :expires     => posting["expires"],
+        :indexed     => posting["indexed"], 
+        :exists      => posting["exists"], 
+        :status      => posting["status"],
+        :annotations => posting["annotations"], 
+        :errors      => posting["errors"], 
+        :history     => posting["history"]                                    
+        })
+    end    
+    res
   end
+
 end
