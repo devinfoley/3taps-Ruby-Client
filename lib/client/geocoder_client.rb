d5b02a2248edaf73ddc2b3f03079c434eb500734
Bratashov Aleksandr <abratashov@gmail.com>
2011-03-11 20:42:59 +0200

    Fix geocoder

--- lib/client/geocoder_client.rb
+++ lib/client/geocoder_client.rb
@@ -3,7 +3,7 @@ class GeocoderClient < Client
   def geocode(string)
     params = "data=#{string}"
     response = execute_get('geocoder/geocode', params)
-    ActiveSupport::JSON.decode(response)
+    GeocoderResponse.from_json(ActiveSupport::JSON.decode(response))
   end
 
 end
--- lib/dto/geocoder/geocoder_response.rb
+++ lib/dto/geocoder/geocoder_response.rb
@@ -3,4 +3,8 @@
 #private Float longitude;
 class GeocoderResponse
   attr_accessor :code, :latitude, :longitude
+
+  def self.from_json(json)
+    self.new
+  end
 end
