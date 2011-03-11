32ab981e5bbe025bbc6df26e7f9d43a5b2d61950
moskov <moskovu@ua.fm>
2011-03-11 16:42:44 +0200

    Summary request

--- lib/dto/search/summary_request.rb
+++ lib/dto/search/summary_request.rb
@@ -4,8 +4,10 @@ class SummaryRequest
   attr_accessor :search_request, :dimension
   
   def query_params
-    query_params = search_request.query_params
-    query_params += "dimension=#{CGI.escape(dimension)}&"
+    query_params = ''
+    query_params = "dimension=#{CGI.escape(dimension)}&" if (dimension != nil)
+    query_params += search_request.query_params if (search_request != nil)
     query_params
   end
+
 end
