90387a2dcce77d3c0e9105dfce4baef32f6558a9
moskov <moskovu@ua.fm>
2011-03-11 16:43:11 +0200

    Summary response

--- lib/dto/search/summary_response.rb
+++ lib/dto/search/summary_response.rb
@@ -2,4 +2,12 @@
 #private Integer execTimeMs;
 class SummaryResponse
   attr_accessor :totals, :exec_time_ms
+
+  def self.from_json(json)
+    results = self.new
+    results.exec_time_ms = json["execTimeMs"]
+    results.totals = json["totals"]
+    results
+  end
+
 end
