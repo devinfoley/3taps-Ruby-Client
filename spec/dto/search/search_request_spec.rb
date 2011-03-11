950d68bffc21491fb0033040c878a79517796817
Bratashov Aleksandr <abratashov@gmail.com>
2011-03-11 17:20:11 +0200

    Fix specs

--- spec/dto/search/search_request_spec.rb
+++ spec/dto/search/search_request_spec.rb
@@ -6,21 +6,22 @@ describe SearchRequest do
   end
 
   it "should return valid query params" do
-    @search_request.rpp = 10
-    @search_request.page = 20
-    @search_request.source = 'SRC'
-    @search_request.category = 'CAT'
-    @search_request.location = 'LOC'
-    @search_request.heading = 'HD'
-    @search_request.body = 'BD'
-    @search_request.text = 'TXT'
-    @search_request.external_id = 'EXID'
-    @search_request.start = Date.new(2011,4,10)
-    @search_request.end = Date.new(2011,4,10)
-    @search_request.annotations = Hash["ANN1", '100', "ANN2", '200']
-    @search_request.trusted_annotations = Hash["TR1", '1000', "TR2", '2000']
-    @search_request.retvals = ["ret1", "ret2", "ret3"]
+    @search_request.rpp = 20
+    @search_request.page = 14
+    #@search_request.source = 'SRC'
+    @search_request.category = 'STOO'
+    #@search_request.location = 'LOC'
+    #@search_request.heading = 'HD'
+    #@search_request.body = 'BD'
+    @search_request.text = 'BMW'
+    #@search_request.external_id = 'EXID'
+    #@search_request.start = Date.new(2011,4,10)
+    #@search_request.end = Date.new(2011,4,10)
+    @search_request.annotations = Hash["Status", 'for+free']
+    #@search_request.trusted_annotations = Hash["TR1", '1000', "TR2", '2000']
+    @search_request.retvals = ["category", "location", "heading", "externalURL", "timestamp", "postKey", "source", "image"]
     @search_request.query_params.should_not == ""
+    p @search_request.query_params
   end
 end
 
