d53388465c01df01e8c28267564a9dcaee717f2a
Bratashov Aleksandr <abratashov@gmail.com>
2011-03-11 20:42:34 +0200

    Fix specs for search

--- spec/client/search_client_spec.rb
+++ spec/client/search_client_spec.rb
@@ -6,12 +6,47 @@ describe SearchClient do
   end
 
   it "should send GET request and create SearchResponse from result" do
-    search_request = mock("search_request")
+    search_request = mock "search_request"
     search_request.should_receive(:query_params)
     search_response = mock "search_response"
     SearchResponse.should_receive(:from_json).and_return search_response
-    
+
     @search_client.search(search_request).should == search_response
   end
+
+  it "should send GET request and create RangeResponse from result" do
+    range_request = mock "range_request"
+    range_request.should_receive(:query_params)
+    range_response = mock "range_response"
+    RangeResponse.should_receive(:from_json).and_return range_response
+
+    @search_client.range(range_request).should == range_response
+  end
+
+  it "should send GET request and create SummaryResponse from result" do
+    summary_request = mock "summary_request"
+    summary_request.should_receive(:query_params)
+    summary_response = mock "summary_response"
+    SummaryResponse.should_receive(:from_json).and_return summary_response
+
+    @search_client.summary(summary_request).should == summary_response
+  end
+  it "should send GET request and create CountResponse from result" do
+    count_request = mock "count_request"
+    count_request.should_receive(:query_params)
+    count_response = mock "count_response"
+    CountResponse.should_receive(:from_json).and_return count_response
+
+    @search_client.count(count_request).should == count_response
+  end
+  it "should send GET request and create CountResponse from result" do
+    bestmatch_request = mock "bestmatch_request"
+    bestmatch_request.should_receive(:query_params)
+    bestmatch_response = mock "bestmatch_response"
+    BestMatchResponse.should_receive(:from_json).and_return bestmatch_response
+
+    @search_client.best_match(bestmatch_request).should == bestmatch_response
+  end
+
 end
 
