9d44f723c48fb3b2da23932ea29408fc05eedc3f
marat <marat@svitla.com>
2011-03-14 22:16:47 +0200

    tests

--- test/client/test_reference_client.rb
+++ test/client/test_reference_client.rb
@@ -2,18 +2,29 @@ require 'helper'
 
 class TestReferenceClient < Test::Unit::TestCase
   should "test get categories" do
-
+    client = ReferenceClient.new
+    categories = client.get_categories
+    assert_equal Array, categories.class
+    assert_equal Category, categories.first.class
   end
 
   should "test get category" do
-
+    client = ReferenceClient.new
+    category = client.get_category("VAUT")
+    assert_equal "VAUT", category.code
   end
 
   should "test get locations" do
-
+    client = ReferenceClient.new
+    locations = client.get_locations
+    assert_equal Array, locations.class
+    assert_equal Location, locations.first.class
   end
 
   should "test get sources" do
-
+    client = ReferenceClient.new
+    sources = client.get_sources
+    assert_equal Array, sources.class
+    assert_equal Source, sources.first.class
   end
 end
--- test/client/test_search_client.rb
+++ test/client/test_search_client.rb
@@ -20,6 +20,8 @@ class TestSearchClient < Test::Unit::TestCase
     client = SearchClient.new
     search_response = client.search(search_request)
     assert_equal SearchResponse, search_response.class
+    assert_equal Array, search_response.results.class
+    assert_equal Posting, search_response.results.first.class
   end
 
   should "get the count request result" do
@@ -29,5 +31,24 @@ class TestSearchClient < Test::Unit::TestCase
     client = SearchClient.new
     count_response = client.count(search_request)
     assert_equal CountResponse, count_response.class
+  end
+
+  should "get the summary result" do
+    search_request = SearchRequest.new
+    search_request.category = 'VAUT'
+    search_request.annotations = {:Make => "porsche"}
+    client = SearchClient.new
+    summary_response = client.summary(search_request)
+    assert_equal SummaryResponse, summary_response.class
+  end
+
+  should "get the best match result" do
+    search_request = SearchRequest.new
+    search_request.category = 'VAUT'
+    search_request.annotations = {:Make => "porsche"}
+    client = SearchClient.new
+    best_match_response = client.best_match(search_request)
+    assert_equal BestMatchResponse, best_match_response.class
+    assert_equal 'VAUT', best_match_response.category
   end
 end
