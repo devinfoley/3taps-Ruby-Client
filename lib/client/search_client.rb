c8c17371ae5e9d081e984eed56aed4677c006cc2
marat <marat@svitla.com>
2011-03-14 15:36:38 +0200

    merged search client

--- lib/client/search_client.rb
+++ lib/client/search_client.rb
@@ -3,7 +3,7 @@
 #
 class SearchClient < Client
 
-  # Searches 3taps for postings. Supports the Common Search Criteria. A request 
+  # Searches 3taps for postings. Supports the Common Search Criteria. A request
   # to search with no Common Search Criteria params will return all postings.
   #
   def search(search_request)
@@ -11,10 +11,10 @@ class SearchClient < Client
     SearchResponse.from_json(ActiveSupport::JSON.decode(response))
   end
 
-  # Returns the minium and maximum values currently in 3taps for the given fields 
-  # that match the given Common Search Criteria. 
-  # 
-  # The purpose of the range method is to provide developers with sensible values 
+  # Returns the minium and maximum values currently in 3taps for the given fields
+  # that match the given Common Search Criteria.
+  #
+  # The purpose of the range method is to provide developers with sensible values
   # for range-based UI filters.
   #
   def range(range_request)
@@ -22,11 +22,11 @@ class SearchClient < Client
     RangeResponse.from_json(ActiveSupport::JSON.decode(response))
   end
 
-  # Returns the total number of postings found in 3taps, across the given dimension, 
-  # that match the given Common Search Criteria parameters. For example, 
-  # searching for "text=toyota" across "dimension=source" would return a list of 
-  # all sources in 3taps, along with the number of postings matching the search 
-  # "text=toyota" in that source. You may currently search across dimensions 
+  # Returns the total number of postings found in 3taps, across the given dimension,
+  # that match the given Common Search Criteria parameters. For example,
+  # searching for "text=toyota" across "dimension=source" would return a list of
+  # all sources in 3taps, along with the number of postings matching the search
+  # "text=toyota" in that source. You may currently search across dimensions
   # source, category, and location.
   #
   def summary(summary_request)
@@ -35,17 +35,28 @@ class SearchClient < Client
   end
 
   # Returns the total number of postings that match the given Common Search Criteria.
-  # 
-  def count(count_request)
-    response = execute_get("/search/count", count_request.query_params)
-    CountResponse.from_json(ActiveSupport::JSON.decode(response)["count"])
+  #
+  # request = SearchRequest.new
+  # request.rpp = 10
+  # request.text = "porsche"
+  # client = SearchClient.new
+  # response = client.count(request)
+  # postings_quantity = response.count
+  def count(search_request)
+    response = execute_get("/search/count", search_request.query_params)
+    CountResponse.from_json(ActiveSupport::JSON.decode(response))
   end
 
   # Returns the 3taps category associated with the keywords, along with the number
   # of postings in that category.
   #
-  def best_match(search_request)
-    response = execute_get("/search/bestMatch", search_request.query_params)
+  # keywords = "iPad,Apple,iPhone"
+  # client = SearchClient.new
+  # response = client.count(keywords)
+  # category_code = response.category
+  # postings_quantity = response.num_results
+  def best_match(keywords)
+    response = execute_get("/search/best-match", "keywords=#{keywords}")
     BestMatchResponse.from_json(ActiveSupport::JSON.decode(response))
   end
 
