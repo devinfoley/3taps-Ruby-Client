80a4a4257b0fe0350fe8ba1a89a92ccd341e7bbd
marat <marat@svitla.com>
2011-03-14 14:19:16 +0200

    added docs

--- lib/client/posting_client.rb
+++ lib/client/posting_client.rb
@@ -1,27 +1,28 @@
-# PostingClient class handles all posting routines: getting a posting, creating,
-# deleting, updating and checking if one exists.
+# The PostingClient allows developers to use 3taps Posting API to store and
+# retrieve postings in the 3taps system.
 class PostingClient < Client
 
+  # Returns information about a single posting.
   def get_posting(post_key)
     response = execute_get("/posting/get/" + post_key)
     ActiveSupport::JSON.decode(response)
   end
 
-  # Method create_posting receives one parameter of type Posting, creates
-  # params hash for it and sends via POST.
-  # Returns CreateResponse object
+  # Saves a new posting in 3taps.
   def create_posting(posting)
     params = "posts=#{posting}"
     response = execute_post("/posting/create", params)
     ActiveSupport::JSON.decode(response)
   end
 
+  # Updates postings on 3taps.
   def update_posting(posting)
     params = "data=#{posting}"
     response = execute_post("/posting/update", params)
     ActiveSupport::JSON.decode(response)
   end
 
+  # Deletes postings from 3taps.
   def delete_posting(post_key)
     params = "data=#{CGI.escape(ActiveSupport::JSON.encode([post_key]))}"
     response = execute_post("/posting/delete", params)
