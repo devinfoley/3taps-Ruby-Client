cd9fa96f532b3d83032a4e9d6f0ea74f3b855588
moskov <moskovu@ua.fm>
2011-03-14 14:13:36 +0200

    Add the method from_json

--- lib/dto/posting/delete_response.rb
+++ lib/dto/posting/delete_response.rb
@@ -1,4 +1,10 @@
 #private Boolean success;
 class DeleteResponse
   attr_accessor :success
+
+  def self.from_json(json)
+    results = self.new
+    results.success = json["success"]
+    results
+  end
 end
