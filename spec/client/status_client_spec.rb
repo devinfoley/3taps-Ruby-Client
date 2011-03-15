a96a88a75d96cff6bb920b61e441997ac700dcd8
moskov <moskovu@ua.fm>
2011-03-15 17:32:27 +0200

    Spec test for status_client

--- spec/client/status_client_spec.rb
+++ spec/client/status_client_spec.rb
@@ -10,19 +10,15 @@ describe StatusClient do
   end
 
   it "should send POST request and create GetResponse from result" do
-    status_request = mock "status_request"
-    status_request.should_receive(:to_json_for_status)
-    status_response = mock "status_response"
-    GetResponse.should_receive(:to_json).and_return status_response
-
-    @status_client.get_status(status_request).should == status_response
+    stub_post_and_json_decode
+    GetResponse.should_receive(:from_json).with([])
+    post = mock "Posting" , :to_json_for_status=>""
+    @status_client.get_status(post).class == GetResponse.class#).should be_true
   end
 
   it "should send GET request and create Message from result" do
-    status_response = mock "status_response"
-    Message.should_receive(:from_json).and_return status_response
-
-    @status_client.system_status().should == status_response
+    stub_get_and_json_decode
+    @status_client.system_status.is_a?(Message).should be_true
   end
 
 end
