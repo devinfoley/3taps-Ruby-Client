b3964810832a19653be4f4e548d1d67b87ef696f
moskov <moskovu@ua.fm>
2011-03-15 12:53:15 +0200

    Update status client

--- test/client/test_status_client.rb
+++ test/client/test_status_client.rb
@@ -6,10 +6,22 @@ class TestStatusClient < Test::Unit::TestCase
   end
 
   should "test get status" do
-
+    res = PostingRequest.new
+    posting = []
+    res.source = 'CRAIG'
+    res.externalID= 3434399120
+    posting << res
+    res.source = 'CRAIG'
+    res.externalID = 33334399121
+    posting << res
+    client = StatusClient.new
+    get_response = client.get_status(posting)
+    assert_equal GetResponse, get_response.class
   end
 
   should "test system status" do
-    
+    client = StatusClient.new
+    status_response = client.system_status
+    assert_equal Message, status_response.class
   end
 end
