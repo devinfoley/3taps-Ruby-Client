24594cd2313aeb0711fa8062dc758a5a1ed64daa
moskov <moskovu@ua.fm>
2011-03-16 18:26:54 +0200

    add method from_json

--- lib/dto/posting/get_response.rb
+++ lib/dto/posting/get_response.rb
@@ -10,6 +10,22 @@ class GetResponse
 #    result.history = PostingHistory.new(json["history"]) if json["history"]
 #    result
 #  end
+  def self.from_array(array)
+    mas = []
+    array.collect do |element|
+      result = self.new
+      result.exists = element["exists"]
+      result.externalID = element["externalID"] 
+      result.source = element["source"] 
+      if element["history"] != nil
+        result.history = PostingHistory.new(element["history"])
+      else
+        result.history = nil
+      end
+      mas << result
+    end
+    mas
+  end
 end
 
 
