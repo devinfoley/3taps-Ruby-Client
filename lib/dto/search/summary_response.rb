5fde42fa47d2ad8a5d1f95b4e5252cb00ca62e86
marat <marat@svitla.com>
2011-03-17 13:05:07 +0200

    Struct from hash creation method

--- lib/dto/search/summary_response.rb
+++ lib/dto/search/summary_response.rb
@@ -1,9 +1,6 @@
-#private Map<String, Integer> totals;
-#private Integer execTimeMs;
 class SummaryResponse < Struct.new(:totals, :execTimeMs) do
     def exec_time_ms
       execTimeMs
     end
   end
-  include HashedInitializer
 end
