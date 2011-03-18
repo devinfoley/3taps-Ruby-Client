02fb5434b0de94cbdb01b8afabbe2d5fa0ac9a54
marat <marat@svitla.com>
2011-03-18 13:20:42 +0200

    GeocoderClient doc

--- lib/client/geocoder_client.rb
+++ lib/client/geocoder_client.rb
@@ -1,23 +1,36 @@
-#Class GeocoderClient represents server request of geocoding API (calculate
-#the location to use for a posting based on location-specific details within
-#the posting such as a street address or a latitude and longitude value).
+# Class GeocoderClient represents server request of geocoding API (calculate
+# the location to use for a posting based on location-specific details within
+# the posting such as a street address or a latitude and longitude value).
+#
+# Its methods are used to query API with appropriate requests:
+# geocode(geocoder_requests) - returns array of responses
+#
+# Example:
+#
+#  client = GeocoderClient.new
+#  request = GeocoderRequest.new
+#  request.latitude = '37.77493'
+#  request.longitude = '-122.41942'
+#
+#  response = client.geocode(request)
+#
+#  response.first.code       # => "CAZ"
+#  response.first.latitude   # => 39.77493
+#  response.first.longitude  # => -122.41942
 
 class GeocoderClient < Client
-  #Its methods are used to query API with appropriate requests:
-  # geocode(geocoder_requests) - returns array of responses
+
+  # Method geocode sends geocode request to Geocode API.
+  # Takes array of GeocoderRequest objects as +geocoder_requests+ parameter.
   #
-  #Example:
+  # Examples:
   #
-  # client = GeocoderClient.new
-  # request = GeocoderRequest.new
-  # request.latitude = '37.77493'
-  # request.longitude = '-122.41942'
+  #  request = GeocoderRequest.new
+  #  client.geocode(request) # => [GeocoderResponse]
   #
-  # response = client.geocode(request)
+  #  requests = [GeocoderRequest.new, GeocoderRequest.new]
+  #  client.geocode(requests) # => [GeocoderResponse, GeocoderResponse]
   #
-  # response.first.code => "CAZ"
-  # response.first.latitude => 39.77493
-  # response.first.longitude => -122.41942
   def geocode(geocoder_requests)
     geocoder_requests = [geocoder_requests] unless geocoder_requests.is_a? Array
     params = "data=["
