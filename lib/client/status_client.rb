711a5e7638d50b206e51d9cee60b8bc9a870f401
moskov <moskovu@ua.fm>
2011-03-18 14:57:36 +0200

    Documentation for Class StatusClient

--- lib/client/status_client.rb
+++ lib/client/status_client.rb
@@ -1,3 +1,4 @@
+# == Class StatusClient
 # The StatusClient class provides access to the Status API.
 #
 # The Status API provides access to the status of postings, both inside and
@@ -15,10 +16,33 @@
 # occurred, and any relevant attributes (postKey, errors, etc). Having this
 # information available allows 3taps and sources to provide maximum visibility
 # into their processes so that both can improve data yield.
+#
+# Class StatusClient provides access to the status API of postings
+# server response returns the status of postings, if a posting has been "sent" to the Posting API by an 
+# external source, that external source can optionally send a status of "sent" the Status API.
+#
+# Its methods are used to query API with appropriate requests:
+#
 class StatusClient < Client
-
+  #
   # Send in status events for postings.
-
+  # update_status(postings) - returns message
+  #
+  #  Examples:
+  #
+    #  posting_client = PostingClient.new
+    #  status_client = StatusClient.new
+    #  update_request = StatusUpdateRequest.new
+    #  posting = posting_client.get_posting("BD9FHQC")
+    #  error = Message.new(:code => 602, :message => "UFO posting error")
+    #  posting.status.errors << error
+    #  posting.status.attributes = {:postKey => "TESTKEY", :message => "UFO test message"}
+    #  posting.status.event = 'lost'
+    #  update_response = status_client.update_status(posting)
+    #
+    #  status_response.code        # => "200"
+    #  status_response.message     # => "Update"
+  #
   def update_status(postings)
     postings = [postings] unless postings.is_a? Array
     params ='events=['
@@ -27,9 +51,28 @@ class StatusClient < Client
     response = execute_post("status/update", params)
     Message.new(decode(response))
   end
-
+  #
   # Get status history for postings.
- def get_status(postings)
+  # get_status(postings) - returns array of class GetStatusResponse
+  #
+  #  Examples: 
+  #
+    #  search_request = SearchRequest.new
+    #  search_request.category = 'VAUT'
+    #  search_request.annotations = {:Make => "porsche"}
+    #  search_request.rpp = 2
+    #  search_request.retvals = ["source", "externalID"]
+    #
+    #  client = SearchClient.new
+    #  search_response = client.search(search_request)
+    #  existing_postings = search_response.results
+    #  existing_postings
+    #  clientS = StatusClient.new
+    #  response = clientS.get_status(existing_postings)
+    #
+    #  response[1]      # => [{"exists" => "false"}]
+  #
+  def get_status(postings)
     postings = [postings] unless postings.is_a? Array
     data = "["
     data << postings.collect{|posting| "{#{posting.to_json_for_status_client}}"}.join(',')
@@ -38,8 +81,17 @@ class StatusClient < Client
     response = execute_post("status/get", params)
     GetStatusResponse.from_array(decode(response))
   end
-
+  #
   # Get the current system status.
+  # system_status() - returns message
+  #
+  #  Examples:
+  #
+    #  client = StatusClient.new
+    #  status_response = client.system_status
+    #  status_response.code        # => "200"
+    #  status_response.message     # => "3taps is up and running!"
+  #
   def system_status
     response = execute_get("/status/system")
     Message.new(decode(response))
