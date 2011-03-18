b97ba812a733fe61c80a7a915aa6d5591c6d18d5
Pyotr K <pyotr.koryakin@svitla.com>
2011-03-18 16:54:37 +0200

    fix broken tests

--- test/dto/search/test_best_match_response.rb
+++ test/dto/search/test_best_match_response.rb
@@ -2,8 +2,9 @@ require 'helper'
 
 class TestBestMatchResponse < Test::Unit::TestCase
   should "get numResults attribute" do
-    response = BestMatchResponse.new("numResults" => 200)
+    response = BestMatchResponse.from_hash("numResults" => 200, "category" => "VAULT")
     assert_equal 200, response.numResults
     assert_equal 200, response.num_results
+    assert_equal "VAULT", response.category
   end
 end
