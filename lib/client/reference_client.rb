9aaadfb48a2e9fc919def7225e605198309379b6
Bratashov Aleksandr <abratashov@gmail.com>
2011-03-18 15:37:30 +0200

    documentation ReferenceClient class

--- lib/client/reference_client.rb
+++ lib/client/reference_client.rb
@@ -1,31 +1,58 @@
-#Class ReferenceClient represents server request of "reference information" API
-#ReferenceClient provides a mechanism for accessing the standard "reference
-#information" used by the 3taps system, including locations, categories, and sources.
+# Class ReferenceClient represents server request of reference API (provides a
+# mechanism for accessing the standard "reference information" used by the
+# 3taps system, including locations, categories, and sources).
 #
+# Its methods are used to query API with appropriate requests:
 #
-
+# +get_categories+ - returns the 3taps categories
+# +get_category+(code) - return single category by passing in the code
+# +get_locations+ - returns the 3taps locations
+# +get_sources+ - returns the 3taps sources
+#
 class ReferenceClient < Client
-  
-  #Returns the 3taps categories
+
+  # Method +get_categories+ returns the 3taps categories.
+  #
+  # Examples:
+  #
+  #  client.get_categories # => [Category, Category]
+  #
   def get_categories
     response = execute_get("/reference/category")
     File.new("newfile",  "w+") << response
     Category.from_array(decode(response))
   end
 
-  #You can request a single category by passing in the category code
-  def get_category(category_code)
-    response = execute_get("/reference/category/" + category_code)
+  # Method +get_category+ returns a single category by passing in the category code.
+  # Takes value of code objects as +string+ parameter.
+  # 
+  # Examples:
+  #
+  #  code = 'NYC'
+  #  client.get_category(code) # => Categoty
+  #
+  def get_category(code)
+    response = execute_get("/reference/category/" + code)
     Category.new(decode(response)[0])
   end
 
-  #Returns the 3taps locations
+  # Method +get_locations+ returns the 3taps locations.
+  #
+  # Examples:
+  #
+  #  client.get_locations # => [Location, Location]
+  #
   def get_locations
     response = execute_get("/reference/location")
     Location.from_array(decode(response))
   end
 
-  #Returns the 3taps sources
+  # Method +get_sources+ returns the 3taps sources.
+  #
+  # Examples:
+  #
+  #  client.get_sources # => [Source, Source]
+  #
   def get_sources
     response = execute_get("/reference/source")
     Source.from_array(decode(response))
