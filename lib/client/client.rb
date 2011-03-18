525901975d7064840cd90e9334cb9cc8e56a9dc5
marat <marat@svitla.com>
2011-03-18 17:51:07 +0200

    Client doc

--- lib/client/client.rb
+++ lib/client/client.rb
@@ -1,11 +1,20 @@
+# Base class for 3taps API client classes.
 class Client
   DEFAULT_URL = 'http://3taps.net'
   DEFAULT_API_PORT = 80
+  # Initializes Client class with +baseUrl+ and +port+ parameters. By default 
+  # DEFAULT_URL and DEFAULT_API_PORT are used. Examples:
+  #  Client.new
+  #  Client.new("http://3taps.com", 8080)
   def initialize(baseUrl = DEFAULT_URL, port = DEFAULT_API_PORT)
     @baseURL = baseUrl
     @port = port
   end
 
+  # Executes GET request on URL and port with +path+ and +params+ parameters.
+  # Example:
+  #
+  #  execute_get("/search", "data=data")
   def execute_get( path, params = nil )
     address = params.nil? ? path : path + '?' + params 
     request = Curl::Easy.new("#{@baseURL}:#{@port}" + address) 
@@ -17,6 +26,10 @@ class Client
     request.body_str
   end
 
+  # Executes POST request on URL and port with +path+ and +params+ parameters.
+  # Example:
+  #
+  #  execute_post("search", "data=data")
   def execute_post( path, params = nil )
     c = Curl::Easy.http_post("#{@baseURL}:#{@port}/#{path}", params)
     c.body_str
