13851f661f3f3ca12b0448bf9ce81c1bee70427b
marat <marat@svitla.com>
2011-03-18 17:06:10 +0200

    SummaryResponse doc

--- lib/dto/search/summary_response.rb
+++ lib/dto/search/summary_response.rb
@@ -1,3 +1,17 @@
+# Class SummaryResponse represents server response on +summary+ Search API
+# request. Server response is sent to initializer which creates object with
+# attributes +totals+, +execTimeMs+ accessible via getters:
+# * +totals+
+# * +execTimeMs+
+# * +exec_time_ms+
+#
+# Examples:
+#
+#  response = SummaryResponse.new("totals" => 20, "execTimeMs" => 100)
+#  response.totals       # => 20
+#  response.execTimeMs   # => 100
+#  response.exec_time_ms # => 100
+#
 class SummaryResponse < Struct.new(:totals, :execTimeMs) do
     def exec_time_ms
       execTimeMs
