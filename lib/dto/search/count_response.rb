ea45ddd8bb54694540e253e9a2741c44a94a45e1
marat <marat@svitla.com>
2011-03-18 15:18:52 +0200

    CountResponse doc

--- lib/dto/search/count_response.rb
+++ lib/dto/search/count_response.rb
@@ -1,4 +1,9 @@
-#private Integer count;
+# Class CountResponse represents server response on +count+ Search API
+# request. Server response is sent to initializer which creates objects with
+# attribute +count+ accessible via getter:
+#
+#  response = CountResponse.new("count" => 20)
+#  response.count  # => 20
+#
 class CountResponse < Struct.new(:count)
-  include HashedInitializer
 end
