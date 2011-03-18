eb6e202acde93930834d7ed8497f48f9ac2c2b37
marat <marat@svitla.com>
2011-03-18 15:26:27 +0200

    SearchClient doc update, RangeResponse doc

--- lib/client/search_client.rb
+++ lib/client/search_client.rb
@@ -1,63 +1,82 @@
 # The SearchClient class allows clients to query 3taps Search API for data,
 # and data about data.
 #
+# Its methods are used to query API with appropriate requests:
+#  client = SearchClient.new
+#  client.search(search_request)    # => returns array of SearchResponse objects
+#  client.range(range_request)      # => returns array of RangeResponse objects
+#  client.summary(summary_request)  # => returns array of SummaryResponse objects
+#  client.count(search_request)     # => returns array of CountResponse objects
+#  client.best_match(keywords)      # => returns array of BestMatchResponse objects
 class SearchClient < Client
 
-  # Searches 3taps for postings. Supports the Common Search Criteria. A request
-  # to search with no Common Search Criteria params will return all postings.
+  # Method +search+ searches 3taps for postings. Example:
+  #
+  #  request = SearchRequest.new
+  #  client = SearchClient.new
+  #  client.search(request)     # => SearchResponse
   #
   def search(search_request)
     response = execute_get("/search", search_request.query_params)
     SearchResponse.new(decode(response))
   end
 
-  # Returns the minium and maximum values currently in 3taps for the given fields
-  # that match the given Common Search Criteria.
+  # Method +range+ returns the minium and maximum values currently in 3taps for
+  # the given fields that match the given Common Search Criteria inside of the
+  # RangeResponse object. The purpose of the range method is to provide
+  # developers with sensible values for range-based UI filters.
   #
-  # The purpose of the range method is to provide developers with sensible values
-  # for range-based UI filters.
+  # Examples:
+  #
+  #  request = RangeRequest.new
+  #  client = SearchClient.new(request)
+  #  client.range(request)        # => RangeResponse
   #
   def range(range_request)
     response = execute_get("/search/range", range_request.query_params)
     RangeResponse.from_array(decode(response))
   end
 
-  # Returns the total number of postings found in 3taps, across the given dimension,
-  # that match the given Common Search Criteria parameters. For example,
-  # searching for "text=toyota" across "dimension=source" would return a list of
-  # all sources in 3taps, along with the number of postings matching the search
-  # "text=toyota" in that source. You may currently search across dimensions
-  # source, category, and location.
+  # Method +summary+ returns the total number of postings found in 3taps, across
+  # the given dimension, that match the given Common Search Criteria parameters
+  # inside of the SummaryResponse object. Searching for "text=toyota" across
+  # "dimension=source" would return a list of all sources in 3taps, along with
+  # the number of postings matching the search "text=toyota" in that source.
+  # You may currently search across dimensions source, category, and location.
+  #
+  # Examples:
+  #
+  #  request = SummaryRequest.new
+  #  client = SearchClient.new
+  #  client.summary(request)      # => SummaryResponse
   #
   def summary(summary_request)
     response = execute_get("/search/summary", summary_request.query_params)
     SummaryResponse.from_hash(decode(response))
   end
 
-  # Returns the total number of postings that match the given Common Search Criteria.
+  # Method +count+ returns the total number of postings that match the given
+  # Common Search Criteria represented by SearchRequest. Example:
   #
-  # request = SearchRequest.new
-  # request.rpp = 10
-  # request.text = "porsche"
-  # client = SearchClient.new
-  # response = client.count(request)
-  # postings_quantity = response.count
+  #  request = SearchRequest.new
+  #  client = SearchClient.new
+  #  client.count(request)      # => CountResponse
+  #
   def count(search_request)
     response = execute_get("/search/count", search_request.query_params)
     CountResponse.new(decode(response))
   end
 
-  # Returns the 3taps category associated with the keywords, along with the number
-  # of postings in that category.
+  # Method +best_match+ returns the 3taps category associated with the keywords,
+  # along with the number of postings in that category. Example:
   #
-  # keywords = "iPad,Apple,iPhone"
-  # client = SearchClient.new
-  # response = client.count(keywords)
-  # category_code = response.category
-  # postings_quantity = response.num_results
+  #  keywords = "iPad,Apple,iPhone"
+  #  client = SearchClient.new
+  #  client.count(keywords)     # => BestMatchResponse
+  # 
   def best_match(keywords)
     response = execute_get("/search/best-match", "keywords=#{keywords}")
-    BestMatchResponse.new(decode(response))
+    BestMatchResponse.from_hash(decode(response))
   end
 
 end
--- lib/dto/search/range_response.rb
+++ lib/dto/search/range_response.rb
@@ -1,6 +1,25 @@
+# Class RangeResponse represents server response on +range+ Search API
+# request. Server response is sent to +from_array+ method which creates objects
+# with attribute +ranges+ accessible via getter:
+#
+#  response = RangeResponse.from_array(...)
+#  response.ranges     # => Array
+#
 class RangeResponse < Struct.new(:ranges) 
+  
+  # Class Range represents elements of server response on +range+ Search API
+  # request. Server response is sent to initializer which creates object
+  # with attributes +field+, +min+, +max+ accessible via getters:
+  #
+  #  range = Range.new(...)
+  #  range.field   # => Array
+  #  range.min     # => 10
+  #  range.max     # => 20
+  #
   class Range < Struct.new(:field, :min, :max)
   end
+
+  # Method +from_array+ creates RangeResponse object with a set of Range objects.
   def self.from_array(json)
     self.from_hash(:ranges => json.collect { |key, value| Range.from_hash( :field => key, :max => value["max"], :min => value["min"])})
   end
